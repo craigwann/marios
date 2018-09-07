7556fdce493b9183e48e46a8608f1807627a4c69
craigwann <craigwann@me.com>
2018-09-07 16:06:09 -0700

    finish CRUD for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,7 +43,7 @@ class ProductsController < ApplicationController
 
 private
   def product_params
-    params.require(:product).permit(:name)
+    params.require(:product).permit(:name, :country, :price)
   end
 
 end
