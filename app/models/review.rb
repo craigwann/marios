dcf178940231dc0fc85876f2fd650172ae38e9b9
craigwann <craigwann@me.com>
2018-09-07 16:23:54 -0700

    pass 4 rspec tests for table associations

---
+++ app/models/review.rb
@@ -0,0 +1,5 @@
+class Review < ActiveRecord::Base
+  belongs_to :product
+
+  validates :author, :presence => true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_215033) do
+ActiveRecord::Schema.define(version: 2018_09_07_230714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2018_09_07_215033) do
     t.string "name"
     t.string "country"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.string "content_body"
+    t.integer "rating"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
